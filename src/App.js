335fffeeacd329831cdb6e5b17cce30fb9f35ae2
yeonjae <pyj3740@gmail.com>
2024-11-26 17:04:52 +0900

    feat: 로그인 router
    로그인 페이지 작성
    이메일과 패스워드를 입력하면 토큰 전달
    App.js에서 렌더링

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./routes/signup";
+import SignIn from "./routes/Signin";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/signin" element={<SignIn />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/routes/Signin.js
@@ -0,0 +1,48 @@
+function SignIn() {
+  function onSubmit(event) {
+    event.preventDefault();
+    const signinForm = event.target;
+    // console.log(event.target);
+    // console.log(event.target["signin-email"].value);
+    fetch("http://127.0.0.1:8000/api/v1/accounts/signin/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: JSON.stringify({
+        email: signinForm["signin-email"].value,
+        password: signinForm["signin-password"].value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        signinForm.reset();
+      });
+  }
+  return (
+    <div>
+      <form id="signin-form" onSubmit={onSubmit}>
+        <label htmlFor="signin-email">email : </label>
+        <input
+          id="signin-email"
+          name="signin-email"
+          type="email"
+          placeholder="email"
+          required
+        />
+        <br />
+        <label htmlFor="signin-password">password : </label>
+        <input
+          id="signin-password"
+          name="signin-password"
+          type="password"
+          placeholder="password"
+          required
+        />
+        <br />
+        <button onclick={onSubmit}>Sign In</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignIn;

7daa2839681368309cc4b5382559f559377b6211
yeonjae <pyj3740@gmail.com>
2024-11-30 00:12:32 +0900

    feat: 프로필 페이지
    개인 프로필 페이지에서 게시글 보여주기

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,20 +1,26 @@
 import react from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function Profile() {
   const { nickname } = useParams();
   const [loading, setLoading] = useState(true);
   const [userInfo, setUserInfo] = useState([]);
+
   const getProfile = async () => {
     const user = await (
       await fetch(`http://127.0.0.1:8000/api/v1/accounts/${nickname}/`)
     ).json();
     setUserInfo(user);
     setLoading(false);
+    console.log(user);
     return;
   };
-  useEffect(() => {getProfile()}, []);
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
     <div>
       {loading ? (
@@ -24,8 +30,39 @@ function Profile() {
           <h3>
             <strong>{userInfo.nickname}</strong>'s Profile
           </h3>
-          <p>{nickname}</p>
-          <p>email : {userInfo.email}</p>
+          {/* <p>{nickname}</p>
+          <p>email : {userInfo.email}</p> */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {userInfo.posts.map((post) => (
+              <div
+                key={post.id}
+                style={{
+                  width: "300px",
+                  height: "300px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <img
+                  style={{ width: "100%" }}
+                  src="https://velog.velcdn.com/images/woowoon920/post/2c808829-e449-4e43-814d-04ed70c5557f/image.png"
+                  alt=""
+                />
+                <div>
+                  <span> ❤️ {post.like_count} </span>
+                  <span> 💬 {post.comments.length}</span>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>

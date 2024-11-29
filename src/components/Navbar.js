8c7f0070e49a48ec8023a4db05494f7e52ac0ed4
yeonjae <pyj3740@gmail.com>
2024-11-29 18:55:26 +0900

    feat : 로그아웃
    로컬스토리지의 refreshToken 전달
    로컬스토리지 초기화

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const getLogin = () => {
+    if (localStorage.getItem("accessToken")) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  };
+
+  const logoutBtn = async () => {
+    await fetch("http://127.0.0.1:8000/api/v1/accounts/signout/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: JSON.stringify({
+        refresh: localStorage.getItem("refreshToken"),
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        localStorage.clear();
+        getLogin();
+      });
+  };
+
+  useEffect(() => {
+    getLogin();
+  }, []);
+
   return (
     <div
       style={{
@@ -17,18 +47,36 @@ function Navbar() {
       >
         Home
       </Link>
-      <Link
-        to="/signup"
-        style={{ textDecoration: "none", color: "blue", cursor: "pointer" }}
-      >
-        signup
-      </Link>
-      <Link
-        to="/signin"
-        style={{ textDecoration: "none", color: "blue", cursor: "pointer" }}
-      >
-        signin
-      </Link>
+      {isLogin ? (
+        <div>
+          로그아웃
+          <button
+            style={{
+              background: "transparent",
+              border: "none",
+              cursor: "pointer",
+            }}
+            onClick={logoutBtn}
+          >
+            signout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <Link
+            to="/signup"
+            style={{ textDecoration: "none", color: "blue", cursor: "pointer" }}
+          >
+            signup
+          </Link>{" "}
+          <Link
+            to="/signin"
+            style={{ textDecoration: "none", color: "blue", cursor: "pointer" }}
+          >
+            signin
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
